6fe875bc3a9f51c00eb0dd573784a518460883a3
Hana Čopáková <hanacopakova@gmail.com>
2021-09-22 23:25:58 +0200

    first commit

---
+++ assets/main.2285f26f.js
@@ -0,0 +1,1 @@
+import{S as e}from"./vendor.022bbab7.js";new e(".splide",{type:"loop",speed:500,perMove:1,perPage:3,gap:10}).mount();
---
+++ assets/vinny-listek.2099ebb4.js
@@ -0,0 +1,1 @@
+import"./main.2285f26f.js";import"./vendor.022bbab7.js";
